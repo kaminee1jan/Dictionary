461d772bd5d34ac13ed1ef8b2cab2cd418b8cbd8
kaminee1jan <83804073+kaminee1jan@users.noreply.github.com>
2021-07-07 11:07:35 +0530

    Add files via upload

---
+++ nested_dictiniory_key_value+itrete.py
@@ -0,0 +1,8 @@
+students = {'Aex':{'class':'V',
+    'rolld_id':2},
+    'Puja':{'class':'V',
+    'roll_id':3}}
+for key,value in students.items():
+    print(key)
+    for i in value:
+        print(i,":",value[i])
