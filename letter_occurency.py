1f997aaa3e8cf4fffadedbdf4ea40c69effc1cee
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2021-07-30 10:35:51 +0530

    Add files via upload

---
+++ letter_occurency.py
@@ -0,0 +1,45 @@
+# st=input("enter the string")
+# dic={}
+# for x in st:
+#     if x in dic:
+#         dic[x]=dic[x]+1
+#     else:
+#         dic[x]=1
+# print(dic)
+str=input("enter the name")
+empty_dict={}
+i=0
+while i<len(str):
+    empty_dict[str[i]]=i
+    i=i+1
+print(empty_dict)
+
+
+
+# l=[1,2,3,4,5,6,7,8,9,0]
+# s=["one","two","three","four","five","six","seven","eight","nine","zero"]
+# user=int(input("enter the number"))
+# list1=[]
+# i=0
+# while i<len(l):
+#     j=0
+#     while j<len(s):
+#         a=s[j]
+#         list1.append(a)
+#         j=j+1
+    # i=i+1
+# print(list1)
+# a=zip(l,s)
+# user=int(input("enter the number"))
+# list1=[]
+# i=0
+# while i<len(l):
+#     if user%10==l[i]:
+#         a=s[i]
+#     if user//10==l[i]:
+#         a=s[l]
+#     i=i+1
+#     n=
+#     list1.append(n)
+# print(list1)
+# print(n)
